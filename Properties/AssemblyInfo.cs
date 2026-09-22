999716cc5383b622304da24f831a23d6cbb4d0b6
neuclean <332666891+neuclean@users.noreply.github.com>
2026-09-23 00:21:42 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KbdReset")]
+[assembly: AssemblyDescription("Releases all keyboard keys in Windows.")]
+[assembly: AssemblyCompany("neuclean@proton.me")]
+[assembly: AssemblyProduct("KbdReset")]
+[assembly: AssemblyCopyright("neuclean@proton.me")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d3e40e20-1995-4b84-befd-902073f20a6d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
